9f782e2db08b602ac1cf02018d62fde0f5598e5a
sonots <sonots@gmail.com>
2014-08-01 19:45:52 +0900

    support measure_mode

--- lib/fluent/rubyprof.rb
+++ lib/fluent/rubyprof.rb
@@ -1,89 +1,103 @@
 require 'optparse'
 require 'drb/drb'
 
-def parse_options
-  op = OptionParser.new
-  op.banner += ' <start/stop> [output_file]'
+module Fluent
+  class Rubyprof
+    def parse_options(argv = ARGV)
+      op = OptionParser.new
+      op.banner += ' <start/stop> [output_file]'
 
-  (class<<self;self;end).module_eval do
-    define_method(:usage) do |msg|
-      puts op.to_s
-      puts "error: #{msg}" if msg
-      exit 1
-    end
-  end
+      (class<<self;self;end).module_eval do
+        define_method(:usage) do |msg|
+          puts op.to_s
+          puts "error: #{msg}" if msg
+          exit 1
+        end
+      end
 
-  opts = {
-    host: '127.0.0.1',
-    port: 24230,
-    unix: nil,
-    command: nil, # start or stop
-    output: '/tmp/fluent-rubyprof.txt'
-  }
-
-  op.on('-h', '--host HOST', "fluent host (default: #{opts[:host]})") {|v|
-    opts[:host] = v
-  }
-
-  op.on('-p', '--port PORT', "debug_agent tcp port (default: #{opts[:host]})", Integer) {|v|
-    opts[:port] = v
-  }
-
-  op.on('-u', '--unix PATH', "use unix socket instead of tcp") {|v|
-    opts[:unix] = v
-  }
-
-  op.on('-o', '--output PATH', "output path (default: #{opts[:output]})") {|v|
-    opts[:output] = v
-  }
-
-  begin
-    op.parse!(ARGV)
-    opts[:command] = ARGV.shift
-    unless %w[start stop].include?(opts[:command])
-      usage "`start` or `stop` must be specified as the 1st argument"
-    end
-  rescue
-    usage $!.to_s
-  end
-
-  opts
-end
-
-def main
-  opts = parse_options
-
-  unless opts[:unix].nil?
-    uri = "drbunix:#{opts[:unix]}"
-  else
-    uri = "druby://#{opts[:host]}:#{opts[:port]}"
-  end
-
-  $remote_engine = DRb::DRbObject.new_with_uri(uri)
-
-  case opts[:command]
-  when 'start'
-    remote_code = <<-CODE
-      require 'ruby-prof'
-      RubyProf.start
-    CODE
-  when 'stop'
-    remote_code = <<-"CODE"
-      result = RubyProf.stop
-      File.open('#{opts[:output]}', 'w') {|f|
-        RubyProf::FlatPrinter.new(result).print(f)
+      opts = {
+        host: '127.0.0.1',
+        port: 24230,
+        unix: nil,
+        command: nil, # start or stop
+        output: '/tmp/fluent-rubyprof.txt',
+        measure_mode: 'PROCESS_TIME',
       }
-    CODE
-  end
 
-  $remote_engine.method_missing(:instance_eval, remote_code)
+      op.on('-h', '--host HOST', "fluent host (default: #{opts[:host]})") {|v|
+        opts[:host] = v
+      }
 
-  case opts[:command]
-  when 'start'
-    $stdout.puts 'fluent-rubyprof: started'
-  when 'stop'
-    $stdout.puts "fluent-rubyprof: outputs to #{opts[:output]}"
+      op.on('-p', '--port PORT', "debug_agent tcp port (default: #{opts[:host]})", Integer) {|v|
+        opts[:port] = v
+      }
+
+      op.on('-u', '--unix PATH', "use unix socket instead of tcp") {|v|
+        opts[:unix] = v
+      }
+
+      op.on('-o', '--output PATH', "output path (default: #{opts[:output]})") {|v|
+        opts[:output] = v
+      }
+
+      op.on('-m', '--measure_mode MEASURE_MODE', "ruby-prof measure mode (default: #{opts[:measure_mode]})") {|v|
+        opts[:measure_mode] = v
+      }
+
+      op.parse!(argv)
+
+      opts[:command] = argv.shift
+      unless %w[start stop].include?(opts[:command])
+        raise OptionParser::InvalidOption.new("`start` or `stop` must be specified as the 1st argument")
+      end
+
+      measure_modes = %w[PROCESS_TIME WALL_TIME CPU_TIME ALLOCATIONS MEMORY GC_RUNS GC_TIME] 
+      unless measure_modes.include?(opts[:measure_mode])
+        raise OptionParser::InvalidOption.new("-m allows one of #{measure_modes.join(', ')}")
+      end
+
+      opts
+    end
+
+    def run
+      begin
+        opts = parse_options
+      rescue OptionParser::InvalidOption => e
+        usage e.message
+      end
+
+      unless opts[:unix].nil?
+        uri = "drbunix:#{opts[:unix]}"
+      else
+        uri = "druby://#{opts[:host]}:#{opts[:port]}"
+      end
+
+      $remote_engine = DRb::DRbObject.new_with_uri(uri)
+
+      case opts[:command]
+      when 'start'
+        remote_code = <<-CODE
+        require 'ruby-prof'
+        RubyProf.measure_mode = eval("RubyProf::#{opts[:measure_mode]}")
+        RubyProf.start
+        CODE
+      when 'stop'
+        remote_code = <<-"CODE"
+        result = RubyProf.stop
+        File.open('#{opts[:output]}', 'w') {|f|
+          RubyProf::FlatPrinter.new(result).print(f)
+        }
+        CODE
+      end
+
+      $remote_engine.method_missing(:instance_eval, remote_code)
+
+      case opts[:command]
+      when 'start'
+        $stdout.puts 'fluent-rubyprof: started'
+      when 'stop'
+        $stdout.puts "fluent-rubyprof: outputs to #{opts[:output]}"
+      end
+    end
   end
 end
-
-main
--- spec/fluent-rubyprof/rubyprof_spec.rb
+++ spec/fluent-rubyprof/rubyprof_spec.rb
@@ -1,28 +1,45 @@
 require 'json'
 require 'spec_helper'
+require 'fluent/rubyprof'
 
-describe 'Fluent::Rubyprof' do
+describe Fluent::Rubyprof do
   CONFIG_PATH = File.join(File.dirname(__FILE__), 'fluent.conf')
   BIN_DIR = File.join(ROOT, 'bin')
   OUTPUT_FILE = File.join(File.dirname(__FILE__), 'test.txt')
 
-  before :all do
-    @fluentd_pid = spawn('fluentd', '-c', CONFIG_PATH, out: '/dev/null')
-    sleep 2
+  context '#parse_options' do
+    it 'incorrect subcommand' do
+      expect { Fluent::Rubyprof.new.parse_options(['foo']) }.to raise_error(OptionParser::InvalidOption)
+    end
 
-    system("#{File.join(BIN_DIR, 'fluent-rubyprof')} start")
-    sleep 2
+    it 'correct measure_mode' do
+      expect { Fluent::Rubyprof.new.parse_options(['start', '-m', 'CPU_TIME']) }.not_to raise_error
+    end
 
-    system("#{File.join(BIN_DIR, 'fluent-rubyprof')} stop -o #{OUTPUT_FILE}")
-    sleep 1
+    it 'incorrect measure_mode' do
+      expect { Fluent::Rubyprof.new.parse_options(['start', '-m', 'foo']) }.to raise_error(OptionParser::InvalidOption)
+    end
   end
 
-  after :all do
-    Process.kill(:TERM, @fluentd_pid)
-    Process.waitall
-  end
+  context 'profiling' do
+    before :all do
+      @fluentd_pid = spawn('fluentd', '-c', CONFIG_PATH, out: '/dev/null')
+      sleep 2
 
-  it 'outputs profiling result' do
-    expect(File.size?(OUTPUT_FILE)).to be_truthy
+      system("#{File.join(BIN_DIR, 'fluent-rubyprof')} start")
+      sleep 2
+
+      system("#{File.join(BIN_DIR, 'fluent-rubyprof')} stop -o #{OUTPUT_FILE}")
+      sleep 1
+    end
+
+    after :all do
+      Process.kill(:TERM, @fluentd_pid)
+      Process.waitall
+    end
+
+    it 'should output' do
+      expect(File.size?(OUTPUT_FILE)).to be_truthy
+    end
   end
 end
